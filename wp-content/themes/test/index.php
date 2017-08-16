7bfbff18ef8e8f1758dc9639b1c905e93fab5458
Quan Vuong <Quan.Vuong@baruch.cuny.edu>
2017-08-16 11:54:55 -0400

    add first blog post

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -4,10 +4,36 @@
 		<meta charset="<?php bloginfo("charset"); ?>">
 		<title><?php bloginfo("name"); ?></title>
 		<link rel="stylesheet" href="<?php bloginfo("stylesheet_url"); ?>">
+		<!-- For future plugins -->
+		<?php wp_head(); ?>		
 	</head>
 
 	<body>
-		
+		<header>
+			<h1><?php bloginfo("name"); ?></h1>
+			<span><?php bloginfo("description"); ?></span>
+		</header>
+
+		<!-- MAIN LOOP -->
+		<div class="main">
+			<!-- fetch all blog posts -->
+			<?php if(have_posts()) : ?>
+				<?php while(have_posts()): the_post(); ?>
+					<h3><?php the_title(); ?></h3>
+					<div class="meta">
+						Created By <?php the_author(); ?> on <?php the_time("F j, Y g:i a");; ?>
+					</div>
+					<?php the_content(); ?>
+				<?php endwhile; ?>
+			<?php else : ?>
+				<?php echo wpautop("Sorry, No Posts Were Found."); ?> <!-- line breaks => paragraphs -->
+			<?php endif; ?>
+		</div>
+
+		<footer>
+			<p>&copy; <?php the_date("Y") ?> - <?php bloginfo("name"); ?>
+			</p>
+		</footer>
 	</body>
 
 </html>
