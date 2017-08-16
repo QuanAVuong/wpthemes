67ac6ef5fa938f9eb8b8821e5519dc75047e9940
Quan Vuong <Quan.Vuong@baruch.cuny.edu>
2017-08-16 15:41:59 -0400

    add single.php

--- wp-content/themes/test/single.php
+++ wp-content/themes/test/single.php
@@ -1,1 +1,29 @@
-TEST SINGLE
+<?php get_header(); ?>
+
+	<!-- MAIN LOOP -->
+	<div class="main">
+		<div class="container">
+			<!-- fetch all blog posts -->
+			<?php if(have_posts()) : ?>
+				<?php while(have_posts()): the_post(); ?>
+					<article class="post">
+						<h3>
+							<?php the_title(); ?>
+						</h3>
+						<div class="meta">
+							Created By <?php the_author(); ?> on <?php the_time("F j, Y g:i a");; ?>
+						</div>
+						<?php the_content(); ?>
+						<br>
+
+					</article>
+
+				<?php endwhile; ?>
+			<?php else : ?>
+				<?php echo wpautop("Sorry, No Posts Were Found."); ?> <!-- line breaks => paragraphs -->
+			<?php endif; ?>
+		</div>
+		
+	</div>
+
+<?php get_footer(); ?>
