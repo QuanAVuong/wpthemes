178f8b1ebb08617a6bab5ff2560d31bb13cf8a02
Quan Vuong <Quan.Vuong@baruch.cuny.edu>
2017-08-16 15:16:37 -0400

    separate header footer files

---
+++ wp-content/themes/test/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo("charset"); ?>">
+		<title><?php bloginfo("name"); ?></title>
+		<link rel="stylesheet" href="<?php bloginfo("stylesheet_url"); ?>">
+		<!-- For future plugins -->
+		<?php wp_head(); ?>		
+	</head>
+
+	<body>
+		<header>
+			<div class="container">
+				<h1><?php bloginfo("name"); ?></h1>
+				<span><?php bloginfo("description"); ?></span>
+			</div>
+		</header>
--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -1,49 +1,23 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo("charset"); ?>">
-		<title><?php bloginfo("name"); ?></title>
-		<link rel="stylesheet" href="<?php bloginfo("stylesheet_url"); ?>">
-		<!-- For future plugins -->
-		<?php wp_head(); ?>		
-	</head>
+<?php get_header(); ?>
 
-	<body>
-		<header>
-			<div class="container">
-				<h1><?php bloginfo("name"); ?></h1>
-				<span><?php bloginfo("description"); ?></span>
-			</div>
-		</header>
-
-		<!-- MAIN LOOP -->
-		<div class="main">
-			<div class="container">
-				<!-- fetch all blog posts -->
-				<?php if(have_posts()) : ?>
-					<?php while(have_posts()): the_post(); ?>
-						<h3><?php the_title(); ?></h3>
-						<div class="meta">
-							Created By <?php the_author(); ?> on <?php the_time("F j, Y g:i a");; ?>
-						</div>
-						<?php the_content(); ?>
-					<?php endwhile; ?>
-				<?php else : ?>
-					<?php echo wpautop("Sorry, No Posts Were Found."); ?> <!-- line breaks => paragraphs -->
-				<?php endif; ?>
-			</div>
-			
+	<!-- MAIN LOOP -->
+	<div class="main">
+		<div class="container">
+			<!-- fetch all blog posts -->
+			<?php if(have_posts()) : ?>
+				<?php while(have_posts()): the_post(); ?>
+					<h3><?php the_title(); ?></h3>
+					<div class="meta">
+						Created By <?php the_author(); ?> on <?php the_time("F j, Y g:i a");; ?>
+					</div>
+					<?php the_content(); ?>
+				<?php endwhile; ?>
+			<?php else : ?>
+				<?php echo wpautop("Sorry, No Posts Were Found."); ?> <!-- line breaks => paragraphs -->
+			<?php endif; ?>
 		</div>
+		
+	</div>
 
-		<footer>
-			<div class="container">
-				<p>&copy; <?php the_date("Y") ?> - <?php bloginfo("name"); ?>
-				</p>
-			</div>
-			
-		</footer>
-
-		<?php wp_footer(); ?>
-	</body>
-
-</html>
+<?php get_footer(); ?>
+		
