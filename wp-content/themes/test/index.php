481294f00abf3d430273404621a401757b4a7209
Quan Vuong <Quan.Vuong@baruch.cuny.edu>
2017-08-16 15:39:31 -0400

    add read more buttons

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -6,15 +6,23 @@
 			<!-- fetch all blog posts -->
 			<?php if(have_posts()) : ?>
 				<?php while(have_posts()): the_post(); ?>
-					<h3>
-						<a href="<?php the_permalink(); ?>">
-							<?php the_title(); ?>
+					<article class="post">
+						<h3>
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</h3>
+						<div class="meta">
+							Created By <?php the_author(); ?> on <?php the_time("F j, Y g:i a");; ?>
+						</div>
+						<?php the_content(); ?>
+						<br>
+
+						<a class="button" href="<?php the_permalink(); ?>">
+							Read More
 						</a>
-					</h3>
-					<div class="meta">
-						Created By <?php the_author(); ?> on <?php the_time("F j, Y g:i a");; ?>
-					</div>
-					<?php the_content(); ?>
+					</article>
+
 				<?php endwhile; ?>
 			<?php else : ?>
 				<?php echo wpautop("Sorry, No Posts Were Found."); ?> <!-- line breaks => paragraphs -->
